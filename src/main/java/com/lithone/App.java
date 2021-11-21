c549e7d3e5bdc66b1572ef9e1be1f828c8483313
Warren Kadosh <wkadosh@gmail.com>
2021-11-21 11:28:06 -0500

    Initial commit.

---
+++ src/main/java/com/lithone/App.java
@@ -0,0 +1,13 @@
+package com.lithone;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
